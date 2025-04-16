e98b36fe0ba8b8752a68e90bfbd088d631914bae
Bayardo Lopez <bjlslopez@gmail.com>
2025-04-16 06:32:13 -0600

    Connecting Vue to Django api locally using axios

---
+++ src/components/BlogPosts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="posts_container">
+    <div class="posts_content">
+      <h1>BlogPosts</h1>
+      <ul class="posts_list">
+        <li v-for="post in posts" :key="post.slug">
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.content }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // posts
+      posts: [],
+    }
+  },
+  methods: {
+    async getData() {
+      try {
+        // fetch posts
+        const response = await this.$axios.get('/blog-posts/')
+        // set the data returned as posts
+        console.log(response)
+        this.posts = response.data
+      } catch (error) {
+        // log the error
+        console.log(error)
+      }
+    },
+  },
+  created() {
+    // Fetch tasks on page load
+    this.getData()
+    console.log('hello world')
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import axios
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000'
 
 const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
 
 app.use(router)
 
