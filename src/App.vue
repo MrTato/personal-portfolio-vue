96d61f5bfa7c0468e412585ed16284012b49fb2b
Bayardo Lopez <bjlslopez@gmail.com>
2025-04-28 23:17:57 -0600

    Organized page layout in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import DesktopNavBar from '@/components/Header/DesktopNavBar.vue'
+import MobileNavSideBar from '@/components/Header/MobileNavSideBar.vue'
+import SiteLogo from '@/components/Header/SiteLogo.vue'
 </script>
 
 <template>
-  <RouterView />
+  <header
+    class="fixed top-0 flex w-full items-center justify-between p-8 md:justify-center md:bg-[#181818] md:p-4"
+  >
+    <SiteLogo />
+
+    <!-- The mobile nav side bar controls opening and closing the menu -->
+    <MobileNavSideBar />
+    <!-- The desktop nav bar is only visible starting on tablet devices -->
+    <DesktopNavBar />
+  </header>
+  <main class="mt-32 flex justify-center">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,43 +1,27 @@
 <script setup>
 import BlogListItem from '@/components/Blog/BlogListItem.vue'
-import BlogPosts from '@/components/Blog/BlogPosts.vue'
-import DesktopNavBar from '@/components/Header/DesktopNavBar.vue'
-import MobileNavSideBar from '@/components/Header/MobileNavSideBar.vue'
-import SiteLogo from '@/components/Header/SiteLogo.vue'
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <header
-      class="fixed top-0 flex w-full items-center justify-between p-8 md:justify-center md:bg-[#181818] md:p-4"
-    >
-      <SiteLogo />
-
-      <!-- The mobile nav side bar controls opening and closing the menu -->
-      <MobileNavSideBar />
-      <!-- The desktop nav bar is only visible starting on tablet devices -->
-      <DesktopNavBar />
-    </header>
-    <main class="mt-32 flex w-5/6 flex-col space-y-9 md:w-3/4 lg:w-2/5">
-      <BlogListItem
-        class="w-full border border-orange-500 bg-black"
-        title="Generic title"
-        excerpt="test data"
-        lastUpdated="2025-01-01"
-      />
-      <BlogListItem
-        class="w-full border border-orange-500 bg-black"
-        title="Generic title"
-        excerpt="test data"
-        lastUpdated="2025-01-01"
-      />
-      <BlogListItem
-        class="w-full border border-orange-500 bg-black"
-        title="Generic title"
-        excerpt="test data"
-        lastUpdated="2025-01-01"
-      />
-    </main>
+  <div class="flex w-5/6 flex-col space-y-9 md:w-3/4 lg:w-2/5">
+    <BlogListItem
+      class="w-full border border-orange-500 bg-black"
+      title="Generic title"
+      excerpt="test data"
+      lastUpdated="2025-01-01"
+    />
+    <BlogListItem
+      class="w-full border border-orange-500 bg-black"
+      title="Generic title"
+      excerpt="test data"
+      lastUpdated="2025-01-01"
+    />
+    <BlogListItem
+      class="w-full border border-orange-500 bg-black"
+      title="Generic title"
+      excerpt="test data"
+      lastUpdated="2025-01-01"
+    />
   </div>
 </template>
 
