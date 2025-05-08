d5a6f8a005ba5375f81615e35d89795286a97a35
Bayardo Lopez <bjlslopez@gmail.com>
2025-05-08 06:43:05 -0600

    Added and tested Sentry configuration

--- src/main.js
+++ src/main.js
@@ -37,11 +37,19 @@ import 'prismjs/components/prism-python'
 import 'prismjs/components/prism-markup'
 import 'prismjs/components/prism-css'
 import DOMPurify from 'dompurify'
+import * as Sentry from '@sentry/vue'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
+Sentry.init({
+  app,
+  dsn: 'https://75e7e178b51822137a67f5a4e99d6d42@o4509285461065728.ingest.us.sentry.io/4509285469782016',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+})
 app.config.globalProperties.$axios = axios
 
 app.config.globalProperties.$sanitize = DOMPurify.sanitize
