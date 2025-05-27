1a6225f68a424291eb4ce8e885b3cc788b713f71
Bayardo Lopez <bjlslopez@gmail.com>
2025-05-27 05:30:20 -0600

    Modified styles for elegance and responsiveness in the QuickPersonalBits component

--- src/components/About/PersonalBitCard.vue
+++ src/components/About/PersonalBitCard.vue
@@ -1,8 +1,12 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center rounded-lg bg-[#1E1E1E] p-4 text-center shadow-md transition duration-300 hover:shadow-orange-400/40"
+    class="flex flex-col items-center justify-center rounded-lg bg-[#1E1E1E] p-4 text-center shadow-md"
   >
-    <font-awesome-icon :icon="icon" class="mb-2 text-2xl text-orange-400" />
+    <font-awesome-icon
+      :icon="icon"
+      class="mb-2 text-2xl"
+      :class="title === 'Coding' ? 'text-white' : 'text-orange-400'"
+    />
     <span class="text-base font-medium text-white">{{ title }}</span>
   </div>
 </template>
--- src/components/About/QuickPersonalBits.vue
+++ src/components/About/QuickPersonalBits.vue
@@ -3,21 +3,47 @@
     <h2 class="mb-12 text-center text-3xl font-bold md:text-4xl">Quick Personal Bits</h2>
 
     <div class="grid auto-rows-[100px] grid-cols-2 gap-4 sm:grid-cols-5">
-      <PersonalBitCard class="col-span-2 row-span-3" :icon="['fas', 'gamepad']" title="Gaming" />
-
-      <PersonalBitCard class="col-span-1 row-span-2" :icon="['fas', 'language']" title="Japanese" />
-      <PersonalBitCard class="col-span-2 row-span-2" :icon="['fas', 'language']" title="Spanish" />
+      <PersonalBitCard
+        class="row-span-4 sm:col-span-2 sm:row-span-3"
+        :icon="['fas', 'gamepad']"
+        title="Gaming"
+      />
 
       <PersonalBitCard
-        class="col-span-1 row-span-1 bg-orange-500 transition hover:bg-orange-600"
+        class="row-span-1 sm:col-span-1 sm:row-span-2"
+        :icon="['fas', 'language']"
+        title="Japanese"
+      />
+
+      <PersonalBitCard
+        class="row-span-2 sm:col-span-2"
+        :icon="['fas', 'language']"
+        title="English"
+      />
+
+      <PersonalBitCard
+        class="row-span-2 bg-orange-500 sm:col-span-1 sm:row-span-1"
         :icon="['fas', 'code']"
         title="Coding"
       />
 
-      <PersonalBitCard class="col-span-2 row-span-3" :icon="['fas', 'book']" title="Reading" />
-      <PersonalBitCard class="col-span-2 row-span-2" :icon="['fas', 'language']" title="English" />
+      <PersonalBitCard
+        class="row-span-4 sm:col-span-2 sm:row-span-3"
+        :icon="['fas', 'book']"
+        title="Reading"
+      />
 
-      <PersonalBitCard class="col-span-1 row-span-2" :icon="['fas', 'pen']" title="Journaling" />
+      <PersonalBitCard
+        class="row-span-2 sm:col-span-2"
+        :icon="['fas', 'language']"
+        title="Spanish"
+      />
+
+      <PersonalBitCard
+        class="row-span-1 sm:col-span-1 sm:row-span-2"
+        :icon="['fas', 'pen']"
+        title="Journaling"
+      />
     </div>
   </section>
 </template>
